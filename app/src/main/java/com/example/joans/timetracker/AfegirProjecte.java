7d3d61653b809f4392cb9b480aac4ff7f1735c6d
t0xb1n <t0xb1n@t0xb1ns-MacBook-Pro.local>
2017-01-01 21:11:04 +0100

    Actividad para a/nadir proyecto

---
+++ app/src/main/java/com/example/joans/timetracker/AfegirProjecte.java
@@ -0,0 +1,28 @@
+package com.example.joans.timetracker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AfegirProjecte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afegir_projecte);
+        Button afegirTascaOK = (Button) findViewById(R.id.addTaskOK);
+        afegirTascaOK.setOnClickListener(butoStartListener);
+    }
+
+    private View.OnClickListener butoStartListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(AfegirProjecte.this, LlistaActivitatsActivity.class);
+            startActivity(intent);
+        }
+    };
+
+}
+
+
