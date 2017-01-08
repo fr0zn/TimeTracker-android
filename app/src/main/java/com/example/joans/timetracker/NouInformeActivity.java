4f7d89153f436f0970c68405d255b829e00a286c
Ernesto Rodriguez Gallardo <erodrgal@usersad.everis.int>
2017-01-08 14:10:06 +0100

    layout de informe ok

--- app/src/main/java/com/example/joans/timetracker/NouInformeActivity.java
+++ app/src/main/java/com/example/joans/timetracker/NouInformeActivity.java
@@ -9,9 +9,17 @@ public class NouInformeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nou_informe);
-        Spinner spinnerInforme = (Spinner) findViewById(R.id.period);
-        ArrayAdapter<String> spinnerInformeArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.periods));
-        spinnerInforme.setAdapter(spinnerInformeArrayAdapter);
+        Spinner spinnerInformePeriode = (Spinner) findViewById(R.id.period);
+        ArrayAdapter<String> spinnerInformePeriodeArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.periods));
+        spinnerInformePeriode.setAdapter(spinnerInformePeriodeArrayAdapter);
+
+        Spinner spinnerInformeLong = (Spinner) findViewById(R.id.tipus);
+        ArrayAdapter<String> spinnerInformeLongArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.info_long));
+        spinnerInformeLong.setAdapter(spinnerInformeLongArrayAdapter);
+
+        Spinner spinnerInformeType = (Spinner) findViewById(R.id.format);
+        ArrayAdapter<String> spinnerInformeTypeArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.info_types));
+        spinnerInformeType.setAdapter(spinnerInformeTypeArrayAdapter);
     }
 
 
